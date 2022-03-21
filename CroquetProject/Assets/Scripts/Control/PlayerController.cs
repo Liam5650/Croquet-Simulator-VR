1a210974c8952dc21f2389a76c5f53fcaff0730b
Liam5650 <62405326+Liam5650@users.noreply.github.com>
2022-03-21 16:22:31 -0400

    Added basic VR control structure

--- CroquetProject/Assets/Scripts/Control/PlayerController.cs
+++ CroquetProject/Assets/Scripts/Control/PlayerController.cs
@@ -12,10 +12,26 @@ public class PlayerController : MonoBehaviour
     public Transform avatar; //Parent object transform for all avatar objects (headset, controllers)
     public GameObject leftController, rightController; //Controller Gameobjects
 
+    private VRControls vrControls;
+
     private void Awake()
     {
         instance = this;
+        vrControls = new VRControls();
     }
+
+    //Enable the VR control scheme
+    private void OnEnable()
+    {
+        vrControls.Enable();
+    }
+
+    //Disable the VR control scheme
+    private void OnDisable()
+    {
+        vrControls.Disable();
+    }
+
     void Start()
     {
 
@@ -24,7 +40,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Handle Laser
+        if(vrControls.VRPlayer.Teleport.WasPressedThisFrame() == true)
+        {
+            LaserController.instance.shoot();
+        }
+        if (vrControls.VRPlayer.Teleport.WasReleasedThisFrame() == true)
+        {
+            LaserController.instance.stopshoot();
+        }
     }
 
 }
